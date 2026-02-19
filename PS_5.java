5956bb732d095dfa7dd4ca13d0f0dc80212d61b8
Ayush1289Kumar <ayushkumarpro1289@gmail.com>
2026-02-19 22:20:50 +0530

    Practice set 5 solved.

---
+++ PS_5.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+public class PS_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        /*
+
+        Question 11: Repeat problem 2 using for loop.
+        */
+
+        // 1. WAP Pattern
+        // for (int i=4;i>=1;i--){
+        //     for (int j = 0; j < i; j++) {
+        //         System.out.print("*");}
+        // System.out.println("");
+        // }
+
+        // 2. WAP to sum first n even numbers using a while loop.
+        // int num = 0;
+        // int n,sum=0;
+
+        // System.out.print("Enter value of n: ");
+        // n = sc.nextInt();
+
+        // while (num<=n){
+            
+        //     if (num%2==0){
+        //     sum=sum+num;}
+        //     num++;
+        // }
+        // System.out.println("Sum: "+sum);
+
+        // 3. WAP to print the multiplication table of a given number n.
+        // int n;
+
+        // System.out.print("Enter value of n: ");
+        // n =  sc.nextInt();
+
+        // for (int i =1;i<=10;i++)
+        //     System.out.println(n+" x "+i+" = "+n*i);
+
+        // 4. WAP to print a multiplication table of 10 in reverse order.
+        // for (int i =10;i>=1;i--)
+        //     System.out.println(10+" x "+i+" = "+10*i);
+        
+
+        // 5. WAP to find the factorial of a given number using for loops.
+        // int num,fact=1;
+        // System.out.print("Enter a number: ");
+        // num = sc.nextInt();
+
+        // for (int i =1;i<=num;i++){
+        //     fact = fact*i;
+        // }
+        // System.out.format("Factorial of %d is %d ",num,fact);
+
+        // 6. WAP factorial using while loop
+        // int num;
+        // int i=1,fact=1;
+        // System.out.print("Enter a number: ");
+        // num = sc.nextInt();
+
+        // while (i<=num){
+        //     fact = fact*i;
+        //     i++;
+        // }
+        // System.out.format("Factorial of %d is %d ",num,fact);
+
+        // 7. WAP pattern using while loop
+        // int i=5;
+
+        // while (i>0){
+        //     int j=1;
+        //     while (j<i){
+        //         System.out.print("*");
+        //         j++;
+        //     }
+        //     System.out.println("");
+        //     i--;
+        // }
+
+        // 8. WAP to calculate the sum of the numbers occurring in the multiplication table of 8.
+        // int sum = 0;
+        
+        // for (int i = 1;i<=10;i++)
+        //     sum = sum + (i*8);
+        
+        // System.out.println("Sum: "+sum);
+
+        // 9. WAP to sum first n even numbers using a for loop.
+        // int n,sum=0;
+
+        // System.out.print("Enter a number: ");
+        // n = sc.nextInt();
+
+        // for (int i =1;i<=n;i++)
+        // {
+        //     sum+=2*i;
+        // }
+        // System.out.format("Sum of first %d even number is %d: ",n,sum);
+
+    }   
+}
